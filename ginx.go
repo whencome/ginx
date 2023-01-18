c13c8249a242aab03f80d4f9286b2d92b0389ed5
whencome <twqzy323@163.com>
2023-01-18 16:12:54 +0800

    add a NewBucket function

--- ginx.go
+++ ginx.go
@@ -3,6 +3,7 @@ package ginx
 import (
     "github.com/gin-gonic/gin"
     "github.com/whencome/ginx/api"
+    "github.com/whencome/ginx/bucket"
     "github.com/whencome/ginx/server"
     "github.com/whencome/ginx/types"
     "github.com/whencome/ginx/view"
@@ -26,4 +27,9 @@ func NewApiHandler(r types.Request, l api.HandlerFunc) gin.HandlerFunc {
 // NewPageHandler 创建一个Page处理对象
 func NewPageHandler(r types.Request, t string, l view.HandlerFunc) gin.HandlerFunc {
     return view.NewHandler(t, r, l)
+}
+
+// NewBucket create a bucket
+func NewBucket(r *gin.RouterGroup, handlers ...types.Handler) *bucket.Bucket {
+    return bucket.New(r, handlers...)
 }
