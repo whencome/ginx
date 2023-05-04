54a6ce10c8351727b935c5446b0827e91f361c45
whencome <twqzy323@163.com>
2023-05-04 11:39:00 +0800

    add WithRouterGroup method to bucket so that we can set router group when a bucket already created

--- bucket.go
+++ bucket.go
@@ -1,85 +1,91 @@
 package ginx
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // bucketer a bucket interface
 type bucketer interface {
-    Register()
-    Group(relativePath string, handlers ...gin.HandlerFunc) *gin.RouterGroup
+	Register()
+	Group(relativePath string, handlers ...gin.HandlerFunc) *gin.RouterGroup
 }
 
 // Bucket a bucket is a group of apis, like *gin.RouterGroup
 type Bucket struct {
-    route       *gin.RouterGroup
-    middlewares []gin.HandlerFunc
-    handlers    []Handler
-    buckets     []bucketer
+	routerGroup *gin.RouterGroup
+	middlewares []gin.HandlerFunc
+	handlers    []Handler
+	buckets     []bucketer
 }
 
 // NewBucket create a new bucket
 func NewBucket(r *gin.RouterGroup, handlers ...Handler) *Bucket {
-    b := &Bucket{
-        route:       r,
-        middlewares: make([]gin.HandlerFunc, 0),
-        handlers:    make([]Handler, 0),
-        buckets:     make([]bucketer, 0),
-    }
-    if len(handlers) > 0 {
-        b.handlers = append(b.handlers, handlers...)
-    }
-    return b
+	b := &Bucket{
+		routerGroup: r,
+		middlewares: make([]gin.HandlerFunc, 0),
+		handlers:    make([]Handler, 0),
+		buckets:     make([]bucketer, 0),
+	}
+	if len(handlers) > 0 {
+		b.handlers = append(b.handlers, handlers...)
+	}
+	return b
+}
+
+// WithRouterGroup use the new RouterGroup g replace origin route set
+func (b *Bucket) WithRouterGroup(g *gin.RouterGroup) *Bucket {
+	b.routerGroup = g
+	return b
 }
 
 func (b *Bucket) Register() {
-    if len(b.handlers) > 0 {
-        for _, h := range b.handlers {
-            h.RegisterRoute(b.route)
-        }
-    }
-    if len(b.buckets) > 0 {
-        for _, bb := range b.buckets {
-            bb.Register()
-        }
-    }
+	if len(b.handlers) > 0 {
+		for _, h := range b.handlers {
+			h.RegisterRoute(b.routerGroup)
+		}
+	}
+	if len(b.buckets) > 0 {
+		for _, bb := range b.buckets {
+			bb.Register()
+		}
+	}
 }
 
 func (b *Bucket) Group(relativePath string, handlers ...gin.HandlerFunc) *gin.RouterGroup {
-    return b.route.Group(relativePath, handlers...)
+	return b.routerGroup.Group(relativePath, handlers...)
 }
 
 func (b *Bucket) UseMiddlewares(ms ...gin.HandlerFunc) {
-    if len(ms) == 0 {
-        return
-    }
-    b.route.Use(ms...)
+	if len(ms) == 0 {
+		return
+	}
+	b.routerGroup.Use(ms...)
 }
 
 func (b *Bucket) AddHandler(h Handler) {
-    if b.handlers == nil {
-        b.handlers = make([]Handler, 0)
-    }
-    b.handlers = append(b.handlers, h)
+	if b.handlers == nil {
+		b.handlers = make([]Handler, 0)
+	}
+	b.handlers = append(b.handlers, h)
 }
 
 func (b *Bucket) AddHandlers(hs []Handler) {
-    if b.handlers == nil {
-        b.handlers = make([]Handler, 0)
-    }
-    b.handlers = append(b.handlers, hs...)
+	if b.handlers == nil {
+		b.handlers = make([]Handler, 0)
+	}
+	b.handlers = append(b.handlers, hs...)
 }
 
 func (b *Bucket) AddBucket(b1 bucketer) {
-    if b.buckets == nil {
-        b.buckets = make([]bucketer, 0)
-    }
-    b.buckets = append(b.buckets, b1)
+	if b.buckets == nil {
+		b.buckets = make([]bucketer, 0)
+	}
+	b.buckets = append(b.buckets, b1)
 }
 
 func (b *Bucket) AddBuckets(bs []bucketer) {
-    if b.buckets == nil {
-        b.buckets = make([]bucketer, 0)
-    }
-    b.buckets = append(b.buckets, bs...)
+	if b.buckets == nil {
+		b.buckets = make([]bucketer, 0)
+	}
+	b.buckets = append(b.buckets, bs...)
 }
